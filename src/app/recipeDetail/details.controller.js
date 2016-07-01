67362fd8944976fad35ce4be2201060126358ef6
Hicham ELG <h.elghourfi@gmail.com>
2016-07-01 12:18:20 +0000

    adding edit ingredientLines

--- src/app/recipeDetail/details.controller.js
+++ src/app/recipeDetail/details.controller.js
@@ -9,6 +9,8 @@
   function detailsController($scope, Auth, $state, recipesService, $window, $stateParams) {
     var vm = this;
 
+    vm.editForm = {};
+
     if (!Auth.isLoggedIn()) {
       return $state.go('login');
     }
@@ -22,6 +24,19 @@
         return;
       }
       vm.recipe.ingredientLines.splice($index, 1);
+    };
+
+    vm.editIngredientLines = function(obj, $index){
+      vm.editForm[obj._id] = true;
+      vm.ingredientLines = obj;
+      console.log(vm.editForm);
+      //vm.recipe.ingredientLines.splice($index, 1);
+    };
+
+    vm.saveIngredientLines = function(obj, $index){
+      vm.editForm[obj._id] = false;
+      console.log(vm.recipe.ingredientLines);
+      //vm.recipe.ingredientLines.splice($index, 1);
     };
 
     vm.saveRecipe = function(){
